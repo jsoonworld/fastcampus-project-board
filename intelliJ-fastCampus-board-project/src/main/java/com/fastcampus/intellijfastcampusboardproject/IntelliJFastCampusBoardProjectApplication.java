3ea9104a5aecdd6c66a5a5a8ebdfb5774d8d763e
Jayson <jsoonworld@gmail.com>
2023-01-09 14:10:36 +0900

    intelliJ

---
+++ intelliJ-fastCampus-board-project/src/main/java/com/fastcampus/intellijfastcampusboardproject/IntelliJFastCampusBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.intellijfastcampusboardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliJFastCampusBoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelliJFastCampusBoardProjectApplication.class, args);
+    }
+
+}
---
+++ intelliJ-fastCampus-board-project/src/test/java/com/fastcampus/intellijfastcampusboardproject/IntelliJFastCampusBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.intellijfastcampusboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliJFastCampusBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
